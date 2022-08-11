c1afc41dd7218fcad1bfeee9a7ad0aac21b7b69e
fredcc <chinayx@gmail.com>
2022-08-12 01:53:30 +0800

    编译目标版本5.19

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,4 +15,5 @@
 
 # Add a feed source
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
-git clone https://github.com/tuanqing/install-program package/install-program
+#git clone https://github.com/tuanqing/install-program package/install-program
+sed -i 's/KERNEL_PATCHVER:=5.15/KERNEL_PATCHVER:=5.19/g' openwrt/target/linux/x86/Makefile
